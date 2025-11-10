f94f713e09a7789124f90f2c3328800c3b741f90
Muhammad <pavou9@gmail.com>
2025-11-09 23:33:39 -0800

    Initial Xcode SwiftUI project setup

---
+++ Afflo/AffloApp.swift
@@ -0,0 +1,21 @@
+//
+//  AffloApp.swift
+//  Afflo
+//
+//  Created by Muhammad M on 11/9/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct AffloApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ AffloTests/AffloTests.swift
@@ -0,0 +1,17 @@
+//
+//  AffloTests.swift
+//  AffloTests
+//
+//  Created by Muhammad M on 11/9/25.
+//
+
+import Testing
+@testable import Afflo
+
+struct AffloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
